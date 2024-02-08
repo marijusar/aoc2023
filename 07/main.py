b41cd5a66a89837521b249c415775daa5f4463a2
Marijus <marijus.arliukas@gmail.com>
2024-02-08 19:45:19 +0200

    0702 done

--- 07/main.py
+++ 07/main.py
@@ -2,7 +2,7 @@ import functools
 
 f = open("input.txt")
 lines = [x.strip().split(" ") for x in f.readlines()]
-cards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+cards = ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]
 card_score_index = list(reversed(cards))
 
 def is_two_pair(a) :
@@ -30,7 +30,7 @@ def determine_hand_score(a):
 
         hand[char] += 1
     hand_counts =list(hand.values())
-    hand_max = max(hand_counts)
+    hand_max = max(hand_counts) 
 
     if hand_max > 3 :
         return hand_max + 2
@@ -43,10 +43,47 @@ def determine_hand_score(a):
 
     return hand_max
 
+def determine_hand_score_wildcard(a) :
+    hand= {}
+    j_count = len(list(filter(lambda x: x == "J" ,a)))
+    for char in a :
+        if char not in hand :
+            hand[char] = 1
+            continue
+
+        hand[char] += 1
+    hand_counts =list(hand.values())
+    if "J" in hand :
+        print(hand_counts)
+        j_index = hand_counts.index(j_count)
+        hand_counts.pop(j_index)
+
+    hand_max = 0
+
+    if len(hand_counts ) >0 :
+        hand_max = max(hand_counts)
+    else :
+        return j_count+2
+
+    hand_max_index = hand_counts.index(hand_max)
+    hand_counts[hand_max_index] = hand_max + j_count
+    new_hand_max = hand_max + j_count
+
+
+    if new_hand_max > 3 :
+        return new_hand_max +2
+
+    if new_hand_max == 3 :
+        return 5 if is_full_house(hand_counts) else 4
+
+    if new_hand_max == 2 :
+        return 3 if is_two_pair(hand_counts) else 2
+
+    return new_hand_max
 
 def hand_compare_function(a, b) :
-    hand_a_score = determine_hand_score(a[0])
-    hand_b_score = determine_hand_score(b[0])
+    hand_a_score = determine_hand_score_wildcard(a[0])
+    hand_b_score = determine_hand_score_wildcard(b[0])
 
     if hand_a_score != hand_b_score :
         return hand_a_score - hand_b_score
@@ -65,6 +102,8 @@ def hand_compare_function(a, b) :
 
 def part_one() :
     sorted_lines = sorted(lines, key=functools.cmp_to_key(hand_compare_function))
+    for sorted_line in sorted_lines :
+        print((sorted_line))
     acc = 0 
 
     for i in range(0, len(sorted_lines)) :
@@ -72,10 +111,11 @@ def part_one() :
         acc += num
 
     return acc
+print(part_one())
         
 
 
-print(part_one())
+
 
     
 
