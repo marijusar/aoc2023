ee675aacafc93ec30f3d2e52b71613e4cafa0977
Marijus <marijus.arliukas@gmail.com>
2024-02-24 20:22:25 +0200

    16011602

--- 16/main.py
+++ 16/main.py
@@ -1,9 +1,9 @@
-f = open("./test.txt")
+import copy
+
+f = open("./input.txt")
 grid = [[y for y in x.strip()] for x in f.readlines()]
 
 
-energized = []
-beams = [((0,0), (1, 0))]
 
 beam_direction_map = {
     "w" : (-1 , 0),
@@ -27,58 +27,83 @@ def get_beam_direction(beam) :
         return "n"
 
 
-while len(beams) > 0 :
-    beam = beams.pop(0)
-    current_tile, next_tile = beam
-    x_next, y_next = next_tile
-    direction = get_beam_direction(beam)
+def solve(start) :
+    cp = copy.deepcopy(grid)
+    energized = []
+    beams = [start]
+    beam_starts = []
+    while len(beams) > 0:
+        beam = beams.pop(0)
+        current_tile, next_tile = beam
+        x_next, y_next = next_tile
+        direction = get_beam_direction(beam)
 
-    if ((current_tile[0], current_tile[1], direction)) in energized :
-        continue
+        if ((x_next, y_next)) in beam_starts :
+            continue
+
+        if x_next < 0 or x_next >= len(cp[0]) or y_next < 0 or y_next >= len(cp) :
+            continue
+     
+
+        energized.append((x_next, y_next, direction))
+
+        if cp[y_next][x_next] == "|" and (direction == "e" or direction == "w") :
+            beams.append(((x_next, y_next), (x_next, y_next + 1 )))
+            beam_starts.append((x_next, y_next))
+            beams.append(((x_next, y_next), (x_next, y_next - 1 )))
+
+        elif cp[y_next][x_next] == "-" and (direction == "n" or direction == "s") :
+            beams.append(((x_next, y_next), (x_next + 1, y_next )))
+            beams.append(((x_next, y_next), (x_next - 1, y_next )))
+            beam_starts.append((x_next, y_next))
+
+        elif cp[y_next][x_next] == "/" :
+            if direction == "s" :
+                beams.append(((x_next, y_next), (x_next - 1, y_next)))
+            if direction == "n" :
+                beams.append(((x_next, y_next), (x_next + 1, y_next)))
+            if direction == "e" :
+                beams.append(((x_next, y_next), (x_next, y_next - 1)))
+            if direction == "w" :
+                beams.append(((x_next, y_next), (x_next, y_next + 1)))
+
+        elif cp[y_next][x_next] == '\\' :
+            if direction == "s" :
+                beams.append(((x_next, y_next), (x_next + 1, y_next)))
+            if direction == "n" :
+                beams.append(((x_next, y_next), (x_next - 1, y_next)))
+            if direction == "e" :
+                beams.append(((x_next, y_next), (x_next, y_next + 1)))
+            if direction == "w" :
+                beams.append(((x_next, y_next), (x_next, y_next - 1)))
+        else :
+            x_beam_offset, y_beam_offset = beam_direction_map[direction]
+            beams.append(((x_next, y_next), (x_beam_offset + x_next , y_beam_offset + y_next)))
+
+    for i in energized : 
+        cp[i[1]][i[0]] = "#"
 
 
-    energized.append((current_tile[0], current_tile[1], direction))
-    if x_next < 0 or x_next >= len(grid[0]) or y_next < 0 or y_next >= len(grid) :
-        continue
- 
-    print(current_tile, next_tile)
-    print(direction)
+    return sum([x.count("#") for x in cp])
 
-    if grid[y_next][x_next] == "|" and (direction == "e" or direction == "w") :
-        beams.append(((x_next, y_next), (x_next, y_next + 1 )))
-        beams.append(((x_next, y_next), (x_next, y_next - 1 )))
+part_one = solve(((-1,0), (0,0)))
+print(part_one)
 
-    elif grid[y_next][x_next] == "-" and (direction == "n" or direction == "s") :
-        beams.append(((x_next, y_next), (x_next + 1, y_next )))
-        beams.append(((x_next, y_next), (x_next - 1, y_next )))
+solutions = []
 
-    elif grid[y_next][x_next] == "/" :
-        if direction == "s" :
-            beams.append(((x_next, y_next), (x_next - 1, y_next)))
-        if direction == "n" :
-            beams.append(((x_next, y_next), (x_next + 1, y_next)))
-        if direction == "e" :
-            beams.append(((x_next, y_next), (x_next, y_next - 1)))
-        if direction == "w" :
-            beams.append(((x_next, y_next), (x_next, y_next + 1)))
+for y_idx, y in enumerate(grid) :
+    left_to_right = solve(((-1, y_idx), (0, y_idx)))
+    right_to_left = solve(((len(grid[y_idx]), y_idx), (len(grid[y_idx]) - 1, y_idx)))
+    solutions.append(left_to_right)
+    solutions.append(right_to_left)
 
-    elif grid[y_next][x_next] == '\\' :
-        if direction == "s" :
-            beams.append(((x_next, y_next), (x_next + 1, y_next)))
-        if direction == "n" :
-            beams.append(((x_next, y_next), (x_next - 1, y_next)))
-        if direction == "e" :
-            beams.append(((x_next, y_next), (x_next, y_next + 1)))
-        if direction == "w" :
-            beams.append(((x_next, y_next), (x_next, y_next - 1)))
-    else :
-        x_beam_offset, y_beam_offset = beam_direction_map[direction]
-        beams.append(((x_next, y_next), (x_beam_offset + x_next , y_beam_offset + y_next)))
+for x_index , x in enumerate(grid[0]) :
+    top_to_bottom = solve(((x_index, -1), (x_index, 0)))
+    bottom_to_top = solve(((x_index, len(grid[0])), (x_index, len(grid[0]) - 1)))
+    solutions.append(top_to_bottom)
+    solutions.append(bottom_to_top)
 
 
-print(energized)
+part_two = max(solutions)
+print(part_two)
 
-for i in energized : 
-    grid[i[1]][i[0]] = "#"
-
-print("\n".join(["".join(x) for x in grid]))
